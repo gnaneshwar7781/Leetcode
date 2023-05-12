95d2d243ac37d37434b372ebe6bd616d54d34d12
gnaneshwar7781 <111452516+gnaneshwar7781@users.noreply.github.com>
2023-05-12 23:41:41 +0530

    Create 74. Search a 2D Matrix.py

---
+++ 74. Search a 2D Matrix.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for column in matrix:
+            if target in column:
+                return True
+        return False
